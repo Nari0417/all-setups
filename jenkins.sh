59eadcfb0b670e5cfdcc972ae9ec538079dc9ec7
Nari0417 <narayanareddy9965@gmail.com>
2025-08-20 13:43:59 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -12,4 +12,5 @@ update-alternatives --config java
 
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins.service
+systemctl enable jenkins.service
 systemctl status jenkins.service
